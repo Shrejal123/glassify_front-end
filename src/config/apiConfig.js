15e890987b1cd128eb7740fb20826ba7f4d9d082
shrejal <shrejaljaiswal070@gmail.com>
2025-05-24 19:52:13 +0530

    new_url_added

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // export const API_BASE_URL = "http://localhost:5454"
-export const API_BASE_URL = "https://glassify-backendcode.vercel.app"
+export const API_BASE_URL = "https://glassify-backendcode.onrender.com"
 
 const jwt=localStorage.getItem("jwt")
 
